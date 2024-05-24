e04bc9f999db836778743820b4ee63b6edbcd4c0
vijayS512 <160640039+vijayS512@users.noreply.github.com>
2024-05-24 12:32:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,265 @@
+// DIv1 Song list
+// song1
+let song1 = new Audio ("mp3/chalate chalate.mp3");
+let play1 =document.getElementById("play1");
+
+play1.addEventListener("click", function(){
+    if (song1.paused|| song1.currentTime<=0){
+        song1.play();
+        play1.classList.add("fa-solid fa-play");
+        play1.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song1.pause();
+        play1.classList.remove("fa-solid fa-play");
+        play1.classList.add("fa-solid fa-play");
+    }
+})
+//song2
+let song2 = new Audio("mp3/Dhadak.mp3");
+let play2 = document.getElementById("play2");
+
+play2.addEventListener("click", function(){
+    if (song2.paused|| song2.currentTime<=0){
+        song2.play();
+        play2.classList.add("fa-solid fa-play");
+        play2.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song2.pause();
+        play2.classList.remove("fa-solid fa-play");
+        play2.classList.add("fa-solid fa-play");
+    }
+})
+
+//song2
+let song3 = new Audio("mp3/Kalank Title.mp3");
+let play3 = document.getElementById("play3");
+
+play3.addEventListener("click", function(){
+    if(song3.paused|| song3.currentTime<=0){
+        song3.play();
+        play3.classList.add("fa-solid fa-play");
+        play3.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song3.pause();
+        play3.classList.remove("fa-solid fa-play");
+        play3.classList.add("fa-solid fa-play");
+    }
+})
+
+//song4
+let song4 = new Audio("mp3/hamari adhuri kahani.mp3");
+let play4 = document.getElementById("play4");
+
+play4.addEventListener("click", function(){
+    if (song4.paused|| song4.currentTime<=0){
+        song4.play();
+        play4.classList.add("fa-solid fa-play");
+        play4.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song4.pause();
+        play4.classList.remove("fa-solid fa-play");
+        play4.classList.add("fa-solid fa-play");
+    }
+})
+
+// Div 3 start from here
+//song 5
+
+let song9 = new Audio("mp3/besharmi ki hight.mp3");
+let play9 = document.getElementById("play9");
+
+play9.addEventListener("click", function(){
+    if (song9.paused|| song9.currentTime<=0){
+        song9.play();
+        play9.classList.add("fa-solid fa-play");
+        play9.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song9.pause();
+        play9.classList.remove("fa-solid fa-play");
+        play9.classList.add("fa-solid fa-play");
+    }
+})
+//song 10
+let song10 = new Audio("mp3/mai tera boyfriend.mp3");
+let play10 = document.getElementById("play10");
+
+play10.addEventListener("click", function(){
+    if (song10.paused|| song10.currentTime<=0){
+        song10.play();
+        play10.classList.add("fa-solid fa-play");
+        play10.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song10.pause();
+        play10.classList.remove("fa-solid fa-play");
+        play10.classList.add("fa-solid fa-play");
+    }
+})
+//song 11
+let song11 = new Audio("mp3/dilbar dilbar.mp3");
+let play11 = document.getElementById("play11");
+
+play11.addEventListener("click", function(){
+    if (song11.paused|| song11.currentTime<=0){
+        song11.play();
+        play11.classList.add("fa-solid fa-play");
+        play11.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song11.pause();
+        play11.classList.remove("fa-solid fa-play");
+        play11.classList.add("fa-solid fa-play");
+    }
+})
+//song 12
+let song12 = new Audio("mp3/suraj duba hai.mp3");
+let play12 = document.getElementById("play12");
+
+play12.addEventListener("click", function(){
+    if (song12.paused|| song12.currentTime<=0){
+        song12.play();
+        play12.classList.add("fa-solid fa-play");
+        play12.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song12.pause();
+        play12.classList.remove("fa-solid fa-play");
+        play12.classList.add("fa-solid fa-play");
+    }
+})
+//div4
+let song13 = new Audio("mp3/ved tuza.mp3");
+let play13 = document.getElementById("play13");
+
+play13.addEventListener("click", function(){
+    if (song13.paused|| song13.currentTime<=0){
+        song13.play();
+        play13.classList.add("fa-solid fa-play");
+        play13.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song13.pause();
+        play13.classList.remove("fa-solid fa-play");
+        play13.classList.add("fa-solid fa-play");
+    }
+})
+
+//song 14
+let song14 = new Audio("mp3/makhmali.mp3");
+let play14 = document.getElementById("play14");
+
+play14.addEventListener("click", function(){
+    if (song14.paused|| song14.currentTime<=0){
+        song14.play();
+        play14.classList.add("fa-solid fa-play");
+        play14.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song14.pause();
+        play14.classList.remove("fa-solid fa-play");
+        play14.classList.add("fa-solid fa-play");
+    }
+})
+
+//song15
+let song15 = new Audio("mp3/Man-Dhaga-Dhaga(PaglaSongs).mp3");
+let play15 = document.getElementById("play15");
+
+play15.addEventListener("click", function(){
+    if (song15.paused|| song15.currentTime<=0){
+        song15.play();
+        play15.classList.add("fa-solid fa-play");
+        play15.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song15.pause();
+        play15.classList.remove("fa-solid fa-play");
+        play15.classList.add("fa-solid fa-play");
+    }
+})
+//song16
+let song16 = new Audio("mp3/mitwa.mp3");
+let play16 = document.getElementById("play16");
+
+play16.addEventListener("click", function(){
+    if (song16.paused|| song16.currentTime<=0){
+        song16.play();
+        play16.classList.add("fa-solid fa-play");
+        play16.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song16.pause();
+        play16.classList.remove("fa-solid fa-play");
+        play16.classList.add("fa-solid fa-play");
+    }
+})
+
+//song17
+let song17 = new Audio("mp3/Unstoppabl.mp3e.mp3");
+let play17 = document.getElementById("play17");
+
+play17.addEventListener("click", function(){
+    if (song17.paused|| song17.currentTime<=0){
+        song17.play();
+        play17.classList.add("fa-solid fa-play");
+        play17.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song17.pause();
+        play17.classList.remove("fa-solid fa-play");
+        play17.classList.add("fa-solid fa-play");
+    }
+})
+//song18
+let song18 = new Audio("mp3/Baby.mp3");
+let play18 = document.getElementById("play18");
+
+play18.addEventListener("click", function(){
+    if (song18.paused|| song16.currentTime<=0){
+        song18.play();
+        play18.classList.add("fa-solid fa-play");
+        play18.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song18.pause();
+        play18.classList.remove("fa-solid fa-play");
+        play18.classList.add("fa-solid fa-play");
+    }
+})
+//song19
+let song19 = new Audio("mp3/If You Believe.mp3");
+let play19 = document.getElementById("play19");
+
+play19.addEventListener("click", function(){
+    if (song19.paused|| song19.currentTime<=0){
+        song19.play();
+        play19.classList.add("fa-solid fa-play");
+        play19.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song19.pause();
+        play19.classList.remove("fa-solid fa-play");
+        play19.classList.add("fa-solid fa-play");
+    }
+})
+//song20
+let song20 = new Audio("mp3/At My Worst.mp3");
+let play20 = document.getElementById("play20");
+
+play20.addEventListener("click", function(){
+    if (song20.paused|| song20.currentTime<=0){
+        song20.play();
+        play20.classList.add("fa-solid fa-play");
+        play20.classList.remove("fa-solid fa-play");
+    }
+    else{
+        song20.pause();
+        play20.classList.remove("fa-solid fa-play");
+        play20.classList.add("fa-solid fa-play");
+    }
+})
